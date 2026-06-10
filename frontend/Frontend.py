41e004daeaa7ed3c16fabca91781343a65128ac9
Hung Nguyen <nguyenhuynhquanghung2005@gmail.com>
2026-06-10 23:06:48 +0300

    Update comment for Frontend

--- frontend/Frontend.py
+++ frontend/Frontend.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 from pathlib import Path
 
-# ===== PAGE CONFIG (PHẢI ĐẶT ĐẦU TIÊN) =====
+# ===== PAGE CONFIG =====
 st.set_page_config(
     page_title="EPL Stats Dashboard",
     page_icon="⚽",
